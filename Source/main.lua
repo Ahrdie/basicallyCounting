355bf686f03657790a013512a11df9c11306801d
Robert Deppe <contact@robertdeppe.de>
2022-03-25 18:30:45 +0000

    :construction: Add digit sprite

--- Source/main.lua
+++ Source/main.lua
@@ -1,26 +1,69 @@
+import "CoreLibs/sprites"
+import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+
 local gfx = playdate.graphics
-local x = 0
 
 -- 0 is fully outside, 1 fully inside
-local controlRodPosition = 0
+local accumulatedNumber = 0
 local controlRodSpeed = 0.5
 local reactorfloor = 200
 local reactormiddle = 175
+local baseNumber = 10
+local maximum = 1024
 
-gfx.setColor(gfx.kColorBlack)
+
+local function createDigit(x,y)
+   
+   local digit = gfx.sprite.new()
+   
+    digit:setZIndex(800)
+       
+    digit:setImage(playdate.graphics.image.new('images/decimal'))
+    digit:moveTo(x, y)
+    digit:add()
+   
+   function digit:update()
+       local value = ( accumulatedNumber % baseNumber ) / baseNumber + 0.05
+       print("value " .. value)
+       digit:setCenter(0.5, value)
+   end
+   return digit
+ 
+end
+
+createDigit(100, 100)
 
 function playdate.update()
-    
-    gfx.fillRect(0, 220, 400*(controlRodPosition), 240)
-    --playdate.drawFPS(0,0)
+    gfx.sprite.update()
+    playdate.drawFPS(0,0)
 end
 
 function playdate.cranked(change)
-    gfx.clear()
+    -- gfx.clear()
 
-    local newRodPosition = controlRodPosition + change/360 * controlRodSpeed
-    controlRodPosition = math.max(0,math.min(1, newRodPosition))
-
+    local newRodPosition = accumulatedNumber + change/360 * controlRodSpeed
+    accumulatedNumber = math.max(0,math.min(maximum or 1, newRodPosition))
+    print("accumulatedNumber " .. accumulatedNumber)
+    -- digit.updateClone(accumulatedNumber)
+    
     gfx.drawText("*" .. change .. "*", 4, 4)
-    gfx.drawText("*" .. controlRodPosition .. "*", 4, 20)
+    gfx.drawText("*" .. accumulatedNumber .. "*", 4, 20)
+    
+end
+
+function playdate.gameWillTerminate()
+
+end
+
+function playdate.deviceWillSleep()
+
+end
+
+function saveState()
+
+end
+
+function playdate.gameWillResume()
+
 end

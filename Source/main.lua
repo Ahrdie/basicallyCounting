ab38e46832e90172acd6ef846ca79b187b65e0a4
Robert Deppe <contact@robertdeppe.de>
2022-03-25 22:26:45 +0100

    :construction: working scrolling digits

--- Source/main.lua
+++ Source/main.lua
@@ -6,14 +6,17 @@ local gfx = playdate.graphics
 
 -- 0 is fully outside, 1 fully inside
 local accumulatedNumber = 0
-local controlRodSpeed = 0.5
+local controlRodSpeed = 1
 local reactorfloor = 200
 local reactormiddle = 175
 local baseNumber = 10
 local maximum = 1024
 
+local lastDigitStart = {x = 352, y = 115}
+local digitDistance = 4
 
-local function createDigit(x,y)
+
+local function createDigit(x,y, power)
    
    local digit = gfx.sprite.new()
    digit:setZIndex(800)
@@ -22,9 +25,10 @@ local function createDigit(x,y)
    digit:add()
    
    function digit:update()
-       local value = ( accumulatedNumber % baseNumber ) / baseNumber + 0.05
-       print("value " .. value)
-       digit:setCenter(0.5, value)
+       local value = (accumulatedNumber/(baseNumber ^ power)) % baseNumber
+       local newCenter = value / baseNumber + 0.05
+       -- print("value " .. value)
+       digit:setCenter(0.5, newCenter)
    end
    return digit
  
@@ -33,12 +37,21 @@ end
 local function createForeground()
     local foreground = gfx.sprite.new()
     foreground:setZIndex(1000)
-    foreground:setImage(playdate.graphics.image.new('images/digit-mask'))
+    foreground:setImage(playdate.graphics.image.new('images/counter'))
     foreground:moveTo(200, 120)
     foreground:add()
 end
 
-createDigit(320, 130)
+local function createDigits()
+    for power = 7, 0, -1
+    do
+       print(power)
+    end
+end
+
+createDigits()
+createDigit(375, 140, 0)
+createDigit(330, 140, 1)
 createForeground()
 
 function playdate.update()
@@ -51,11 +64,11 @@ function playdate.cranked(change)
 
     local newRodPosition = accumulatedNumber + change/360 * controlRodSpeed
     accumulatedNumber = math.max(0,math.min(maximum or 1, newRodPosition))
-    print("accumulatedNumber " .. accumulatedNumber)
+    -- print("accumulatedNumber " .. accumulatedNumber)
     -- digit.updateClone(accumulatedNumber)
     
-    gfx.drawText("*" .. change .. "*", 4, 4)
-    gfx.drawText("*" .. accumulatedNumber .. "*", 4, 20)
+    -- gfx.drawText("*" .. change .. "*", 4, 4)
+    -- gfx.drawText("*" .. accumulatedNumber .. "*", 4, 20)
     
 end
 

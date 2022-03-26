32bb4b82a87ed0ac8e389c14a96a7037abb4ffc5
Robert Deppe <contact@robertdeppe.de>
2022-03-26 11:01:46 +0000

    :sparkles: allow switching between bases

--- Source/main.lua
+++ Source/main.lua
@@ -1,6 +1,7 @@
 import "CoreLibs/sprites"
 import "CoreLibs/graphics"
 import "CoreLibs/sprites"
+import "CoreLibs/math"
 
 local gfx = playdate.graphics
 
@@ -9,51 +10,109 @@ local accumulatedNumber = 0
 local controlRodSpeed = 1
 local reactorfloor = 200
 local reactormiddle = 175
-local baseNumber = 10
+local baseSelection = 2
+local base = {16,10,2}
 local maximum = 1024
 
 local lastDigitStart = {x = 352, y = 115}
 local digitGap = 5
 local digitHeight = 60
 
+local speed = 0
 
 local function createDigit(x,y, power)
-   
-   local image = playdate.graphics.image.new('images/decimal-balloon')
-   
+   local baseNumber = base[baseSelection]
    local digit = gfx.sprite.new()
    digit:setZIndex(800 + power)
-   digit:setImage(image)
-   digit:moveTo(x, y)
-   digit:add()
-    
+   
     local shadowBehind = gfx.sprite.new()
     shadowBehind:setZIndex(900 + power)
-    shadowBehind:setImage(image)
-    shadowBehind:moveTo(x, y + digitHeight)
-    shadowBehind:add()
-
-   
+    
    function digit:update()
+       digit:checkBase()
        local value = (accumulatedNumber/(baseNumber ^ power)) % baseNumber
        local newCenter = value / baseNumber
        digit:setCenter(0.5, newCenter)
        shadowBehind:setCenter(0.5, ( value - baseNumber +1) / baseNumber)
    end
+   
+   function digit:setNewBase()
+       baseNumber = base[baseSelection]
+       local image = playdate.graphics.image.new('images/digit' .. baseNumber)
+       digit:setImage(image)
+       shadowBehind:setImage(image)
+   end
+   
+   function digit:checkBase()
+       print((baseNumber == base[baseSelection]) == false)
+       if (baseNumber == base[baseSelection]) == false then
+           digit:setNewBase()
+       end
+   end
+   
+   digit:setNewBase()
+   digit:moveTo(x, y)
+   digit:add()
+    
+    shadowBehind:moveTo(x, y + digitHeight)
+    shadowBehind:add()
+    
    return digit
- 
+end
+
+local function createBaseSign(x,y, baseNumber)
+    local image = playdate.graphics.image.new('images/base' .. baseNumber)
+    local selected = false
+    local extension = 0
+    
+    local middleStart = 0.5
+    local extendedMiddle = 0.9
+       
+    local sign = gfx.sprite.new()
+    sign:setZIndex(1100)
+    sign:setImage(image)
+    sign:moveTo(x, y)
+    sign:add()
+    
+    function sign:update()
+        sign:checkBase()
+        if selected and extension < 1 then
+            extension += 0.05
+            extension = math.min(extension, 1)
+        elseif (not selected) and extension > 0 then
+            extension -= 0.07
+            extension = math.max(extension, 0)
+        end
+        
+        sign:setCenter(0.5, playdate.math.lerp(middleStart, extendedMiddle, extension))
+    end
+    
+    function sign:checkBase()
+        if baseNumber == base[baseSelection] then
+            selected = true
+        else
+            selected = false
+        end
+    end
 end
 
 local function createForeground()
+    local foregroundMask = gfx.sprite.new()
+    foregroundMask:setZIndex(1000)
+    foregroundMask:setImage(playdate.graphics.image.new('images/digitMask'))
+    foregroundMask:moveTo(200, 120)
+    foregroundMask:add()
+    
     local foreground = gfx.sprite.new()
-    foreground:setZIndex(1000)
+    foreground:setZIndex(2000)
     foreground:setImage(playdate.graphics.image.new('images/counter'))
     foreground:moveTo(200, 120)
     foreground:add()
 end
 
 local function createDigits()
-    local image = playdate.graphics.image.new('images/decimal')
+    local image = playdate.graphics.image.new('images/digit10')
+    print(image)
     local width, h = image:getSize()
     
     for power = 0, 7, 1
@@ -63,10 +122,29 @@ local function createDigits()
     end
 end
 
+local function createBaseSigns()
+    print(base)
+    for i, power in ipairs(base) do
+        createBaseSign(350 - i * 35, 109, power)
+    end
+end
+
+gfx.setBackgroundColor(playdate.graphics.kColorBlack)
 createDigits()
+createBaseSigns()
 createForeground()
 
 function playdate.update()
+    if playdate.buttonJustPressed(playdate.kButtonLeft) then
+        print(baseSelection, "left +1")
+        local newBase = baseSelection += 1
+        baseSelection = math.min(newBase, #base)
+    elseif playdate.buttonJustPressed(playdate.kButtonRight) then
+        print(baseSelection, "right -1")
+        local newBase = baseSelection -= 1
+        baseSelection = math.max(newBase, 1)
+    end
+    
     gfx.sprite.update()
     playdate.drawFPS(0,0)
 end
@@ -80,6 +158,7 @@ function playdate.cranked(change)
     -- gfx.drawText("*" .. accumulatedNumber .. "*", 4, 20)
     
 end
+
 
 function playdate.gameWillTerminate()
 
